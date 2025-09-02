593fd4646e1e315ba07fa376121cc5e0749f23fb
Felipe Trevisan <felipejs@gmail.com>
2025-09-01 23:05:24 -0300

    fix: dashboard page

--- apps/web/src/app/(app)/(dashboard)/page.tsx
+++ apps/web/src/app/(app)/(dashboard)/page.tsx
@@ -1,17 +1,9 @@
 import type { Metadata } from 'next'
-import { DashboardWidgets } from './widgets'
 
 export const metadata: Metadata = {
   title: 'Dashboard',
 }
 
-export const revalidate = 900
-
 export default function DashboardPage() {
-  return (
-    <>
-      <h2 className="font-bold text-3xl tracking-tight">Dashboard</h2>
-      <DashboardWidgets />
-    </>
-  )
+  return <></>
 }
